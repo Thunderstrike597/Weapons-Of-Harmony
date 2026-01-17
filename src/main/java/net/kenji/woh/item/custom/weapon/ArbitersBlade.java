fdb634ca34d96e20e789d3253b607b4e4dd389da
Thunderstrike597 <trueblue24001@gmail.com>
2026-01-17 15:35:45 +1000

    Commit For Epic Fight-20.14.4 & 20.9.7 -Adjusted Repair Materials For Weapons

--- src/main/java/net/kenji/woh/item/custom/weapon/ArbitersBlade.java
+++ src/main/java/net/kenji/woh/item/custom/weapon/ArbitersBlade.java
@@ -2,11 +2,14 @@ package net.kenji.woh.item.custom.weapon;
 
 import net.kenji.woh.item.custom.base.HolsterWeaponBase;
 import net.kenji.woh.registry.WohItems;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ClickAction;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.level.Level;
+import org.jline.utils.Log;
 import yesman.epicfight.gameasset.Armatures;
 
 public class ArbitersBlade extends HolsterWeaponBase {
--- src/main/java/net/kenji/woh/registry/WohWeaponTiers.java
+++ src/main/java/net/kenji/woh/registry/WohWeaponTiers.java
@@ -4,24 +4,26 @@ import net.minecraft.util.LazyLoadedValue;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
 
 public enum WohWeaponTiers implements Tier {
-    SHOTOGATANA(4, 2200, 9.0F, 7.0F, 22, () -> Ingredient.of(new ItemLike[]{WohItems.FOLDED_STEEL.get(), WohItems.BROKEN_BLADE_AND_SHEATH.get()})),
-    TESSEN(4, 1750, 9.0F, 4.0F, 18, () -> Ingredient.of(new ItemLike[]{WohItems.FOLDED_STEEL.get()})),
-    TSUME(4, 1750, 9.0F, 5.0F, 18, () -> Ingredient.of(new ItemLike[]{WohItems.FOLDED_STEEL.get()})),
-    ARBITERS_BLADE(4, 1750, 9.0F, 5.0F, 18, () -> Ingredient.of(new ItemLike[]{WohItems.FOLDED_STEEL.get()})),
+    SHOTOGATANA(4, 2200, 9.0F, 7.0F, 22, () -> Ingredient.of(new ItemLike[]{WohItems.FOLDED_STEEL.get(), WohItems.BROKEN_BLADE_AND_SHEATH.get(), WohItems.WEAPON_REPAIR_MODULE.get()})),
+    TESSEN(4, 1750, 9.0F, 4.0F, 18, () -> Ingredient.of(new ItemLike[]{WohItems.FOLDED_STEEL.get(), WohItems.BROKEN_FAN_BLADE.get(), WohItems.WEAPON_REPAIR_MODULE.get()})),
+    TSUME(4, 1750, 9.0F, 5.0F, 18, () -> Ingredient.of(new ItemLike[]{WohItems.FOLDED_STEEL.get(), WohItems.BROKEN_CLAWS.get(), WohItems.WEAPON_REPAIR_MODULE.get()})),
+    ARBITERS_BLADE(4, 1750, 9.0F, 5.0F, 18, () -> Ingredient.of(new ItemLike[]{WohItems.WEAPON_REPAIR_MODULE.get()})),
 
     WAKIZASHI(4, 1750, 9.0F, 5.0F, 18, () -> Ingredient.of(new ItemLike[]{WohItems.FOLDED_STEEL.get()})),
     ODACHI(4, 1750, 9.0F, 11.0F, 18, () -> Ingredient.of(new ItemLike[]{WohItems.FOLDED_STEEL.get()}));
+
 
     private final int harvestLevel;
     private final int maxUses;
     private final float efficiency;
     private final float attackDamage;
     private final int enchantability;
-    private final LazyLoadedValue<Ingredient> repairMaterial;
+    private final Supplier<Ingredient> repairMaterial;
 
 
     WohWeaponTiers(int harvestLevelIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantabilityIn, Supplier<Ingredient> repairMaterialIn) {
@@ -30,7 +32,7 @@ public enum WohWeaponTiers implements Tier {
        this.efficiency = efficiencyIn;
        this.attackDamage = attackDamageIn;
        this.enchantability = enchantabilityIn;
-       this.repairMaterial = new LazyLoadedValue(repairMaterialIn);
+       this.repairMaterial = repairMaterialIn;
    }
 
 
@@ -60,7 +62,7 @@ public enum WohWeaponTiers implements Tier {
     }
 
     @Override
-    public Ingredient getRepairIngredient() {
+    public @NotNull Ingredient getRepairIngredient() {
         return this.repairMaterial.get();
     }
 }
