8237849a038a84e707641ecd6f4740d34717cb69
Thunderstrike597 <trueblue24001@gmail.com>
2026-01-30 09:27:01 +1000

    Commit For Epic Fight-20.14.4 & 20.9.7 -Fixed Holster Incorrect On Weapon Change -Fixed Shield Render On Use

--- src/main/java/net/kenji/woh/item/custom/base/WohShieldItem.java
+++ src/main/java/net/kenji/woh/item/custom/base/WohShieldItem.java
@@ -30,7 +30,7 @@ public class WohShieldItem extends ShieldItem {
     }
     public boolean shouldRenderInHand(PlayerPatch<?> playerPatch, Item item){
         ItemStack holdingItem = playerPatch.getOriginal().getItemInHand(InteractionHand.MAIN_HAND);
-        return (holdingItem.getItem() == item | playerPatch.getOriginal().isUsingItem() & playerPatch.getOriginal().getUseItem().getItem() == this) && playerPatch.isBattleMode();
+        return ((holdingItem.getItem() == item & playerPatch.getOriginal().getUseItem().getItem() == this) && playerPatch.isBattleMode()) || playerPatch.getOriginal().isUsingItem();
     }
 
     public StaticAnimation getBlockAnimation(){
