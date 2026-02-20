9375c3166ae6a0027b92f133012b4a021f7565b6
Thunderstrike597 <trueblue24001@gmail.com>
2026-02-20 12:20:32 +1000

    Commit For Epic Fight-20.9.7 & 20.14.4 -Fixed Beam Slash Entity Persisting across sessions in some scenarios

--- src/main/java/net/kenji/woh/entities/custom/BeamSlashEntity.java
+++ src/main/java/net/kenji/woh/entities/custom/BeamSlashEntity.java
@@ -1,5 +1,6 @@
 package net.kenji.woh.entities.custom;
 
+import net.kenji.woh.WeaponsOfHarmony;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
@@ -13,6 +14,9 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.jline.utils.Log;
 import yesman.epicfight.api.animation.types.StaticAnimation;
 import yesman.epicfight.api.utils.math.ValueModifier;
@@ -32,6 +36,8 @@ public class BeamSlashEntity extends Entity {
             SynchedEntityData.defineId(BeamSlashEntity.class, EntityDataSerializers.FLOAT);
     private static final EntityDataAccessor<Float> Y_ROT =
             SynchedEntityData.defineId(BeamSlashEntity.class, EntityDataSerializers.FLOAT);
+
+    public static Map<UUID, BeamSlashEntity> beamSlashEntitiesToDestory = new HashMap<>();
 
     private PlayerPatch<?> caster;
     private StaticAnimation castAnimation;
@@ -61,6 +67,20 @@ public class BeamSlashEntity extends Entity {
         this.entityData.define(Y_ROT, 0f);
     }
 
+    @Mod.EventBusSubscriber(modid = WeaponsOfHarmony.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class EntityEvents{
+
+        @SubscribeEvent
+        public static void onEntityJoin(EntityJoinLevelEvent event){
+            if(event.loadedFromDisk()){
+                if(event.getEntity() instanceof BeamSlashEntity beamSlashEntity){
+                    BeamSlashEntity.beamSlashEntitiesToDestory.put(beamSlashEntity.uuid, beamSlashEntity);
+                }
+            }
+        }
+    }
+
+
     @Override
     public void setYRot(float pYRot) {
         this.entityData.set(Y_ROT, pYRot);
@@ -77,6 +97,13 @@ public class BeamSlashEntity extends Entity {
 
     public float getSlashAngle() {
         return this.entityData.get(SLASH_ANGLE);
+    }
+
+    @Override
+    public void onAddedToWorld() {
+        super.onAddedToWorld();
+        if(beamSlashEntitiesToDestory.get(this.getUUID()) != null)
+            this.kill();
     }
 
     @Override
